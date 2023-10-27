ac6c709a55c638f5330983287101630a89e5aae5
Matthieu Thiboust <14574229+mthiboust@users.noreply.github.com>
2023-10-27 15:09:19 +0200

    lint & format

--- src/colormap2d/__init__.py
+++ src/colormap2d/__init__.py
@@ -1,1 +1,12 @@
+"""2D colormap.
+
+`matplotlib` provides many colormaps that map scalars to colors. 
+However, it does not provide such colormaps for 2D vectors. 
+Representing 2D vectors as colors may be helpful when dealing with 
+complex numbers or 2D coordinates. This library provides 2 colormaps 
+for this purpose.
+"""
+
 from .cm import pinwheel, cyclic_pinwheel
+
+__all__ = ["pinwheel", "cyclic_pinwheel"]
--- src/colormap2d/cm.py
+++ src/colormap2d/cm.py
@@ -1,41 +1,48 @@
+"""2D colormap.
+
+`matplotlib` provides many colormaps that map scalars to colors. 
+However, it does not provide such colormaps for 2D vectors. 
+Representing 2D vectors as colors may be helpful when dealing with 
+complex numbers or 2D coordinates. This library provides 2 colormaps 
+for this purpose.
+"""
+
 from importlib import resources
 
 import numpy as np
-    
-N = 50 # number of samples inside the colormap data
+
+N = 50  # number of samples inside the colormap data
+
 
 def _load_npy(relative_path):
-    
-    with resources.path(__package__ + '.data', relative_path) as path:
+    with resources.path(__package__ + ".data", relative_path) as path:
         return np.load(path)
 
-def _apply_colormap(arr, colormap):
 
+def _apply_colormap(arr, colormap):
     if not isinstance(arr, np.ndarray):
         raise TypeError(f"Parameter must be a numpy array, not {type(arr)}")
     if arr.shape[-1] != 2:
         raise ValueError(f"Last dimension of array shape {arr.shape} must be 2.")
 
     arr = np.round(arr * N).astype(np.int32)
-    colormap_data = _load_npy(colormap + '.npy')
-    return colormap_data[arr[...,0], arr[...,1]]
+    colormap_data = _load_npy(colormap + ".npy")
+    return colormap_data[arr[..., 0], arr[..., 1]]
+
 
 def pinwheel(arr):
     """Converts 2D coordinates into 3D RGB values.
-    
+
     Args:
         arr: Numpy array whose last dimension is 2 and whose values belong to [0,1]
     """
-
-    return _apply_colormap(arr, 'pinwheel')
+    return _apply_colormap(arr, "pinwheel")
 
 
 def cyclic_pinwheel(arr):
     """Converts 2D coordinates into 3D RGB values which are the same at each XY border.
-    
+
     Args:
         arr: Numpy array whose last dimension is 2 and whose values belong to [0,1]
     """
-
-    return _apply_colormap(arr, 'cyclic_pinwheel')
-
+    return _apply_colormap(arr, "cyclic_pinwheel")
